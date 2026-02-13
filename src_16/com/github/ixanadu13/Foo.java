f09dd217bca031981de49f827505e0dbb326b1f1
Xanadu13 <xanadu13@qq.com>
2026-02-13 17:09:16 +0800

    add test file

---
+++ src_16/com/github/ixanadu13/Foo.java
@@ -0,0 +1,29 @@
+package com.github.ixanadu13;
+
+public class Foo {
+    public void test_switch() {
+        String str = "12";
+        switch (str) {
+            default: {
+
+            }
+        }
+    }
+    public void test_switch2() {
+        int i = 12;
+        switch (i) {
+            case 12: {
+                break;
+            }
+            case 13: {
+                break;
+            }
+            case 14: {
+                break;
+            }
+            default: {
+
+            }
+        }
+    }
+}
